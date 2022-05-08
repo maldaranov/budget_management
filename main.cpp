e71ede76662269fad25b43ce5ab006ff8eb67eb2
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2022-05-08 18:54:01 -0400

    Gets user's full name.

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main () {
     cout << "Hello, user! This program will help you track your earnings, spendings, and total balance." << endl;
+    string user_name;
+    cout << "Please enter your full name: ";
+    getline(cin, user_name);
     return 0;
 }
