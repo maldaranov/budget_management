fd57cd7a7393ac775b953e9a7fd0555cd707e4db
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2022-05-08 18:57:17 -0400

    Adds comments to the 'main.cpp'.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main () {
+    // Greetings message.
     cout << "Hello, user! This program will help you track your earnings, spendings, and total balance." << endl;
+
+    // Get user's full name.
     string user_name;
     cout << "Please enter your full name: ";
     getline(cin, user_name);
+    
     return 0;
 }
