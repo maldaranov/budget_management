6cfa42d738e3d85a294ee868ba4a9c57767392c0
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2022-05-08 18:27:11 -0400

    Adds a greeting message.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "records.h"
+using namespace std;
 
 int main () {
+    cout << "Hello, user! This program will help you track your earnings, spendings, and total balance." << endl;
     return 0;
 }
