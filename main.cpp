2268d645c352acdc8c23270b4366373be6ce7ff3
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2022-05-09 14:18:36 -0400

    Adds user interface loop template with three options.

--- main.cpp
+++ main.cpp
@@ -13,9 +13,31 @@ int main () {
     
     // Initialize the 'User' object.
     User user(user_name);
-    #ifdef generate_file_name
+    #ifdef generate_file_name // Test if the correct file name was generated.
     cout << "This is the generated file name: " << user.get_record_name() << endl;
     #endif
+
+    // User interface.
+    char c;
+    while (1) {
+        cout << "\nPlease enter the desired operation to perform." << endl;
+        cout << " '+' to records earnings." << endl;
+        cout << " '-' to records spendings." << endl;
+        cout << " 'x' to exit the program." << endl;
+        cin >> c;
+        cout << endl;
+
+        if (c == '+') {
+            cout << "Earnings." << endl;
+        } else if (c == '-') {
+            cout << "Spendings." << endl;
+        } else if (c == 'x') {
+            cout << "Exited." << endl;
+            exit(0);
+        } else {
+            cout << "ERROR: invalid input." << endl;
+        }
+    }
 
     return 0;
 }
